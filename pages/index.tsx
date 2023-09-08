e9204cbe480620a2c998689711a9aec63a7f002e
tubi55 <tubi55@nate.com>
2023-09-08 12:55:21 +0900

    ssr방식으로 영화데이터 props로 전달

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import Header from '@/components/Header';
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import requests from '@/utils/request';
 
-const Home: NextPage = () => {
+const Home: NextPage = ({ sf }) => {
+	console.log(sf);
 	return (
 		<div className='relatvie h-screen bg-gradient-to-b from-[#333] to=[#141414]'>
 			<Head>
@@ -18,3 +20,12 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+
+export const getServerSideProps = async () => {
+	const sf = await fetch(requests.sf).then((res) => res.json());
+	console.log(sf);
+
+	return {
+		props: { sf: sf.results },
+	};
+};
